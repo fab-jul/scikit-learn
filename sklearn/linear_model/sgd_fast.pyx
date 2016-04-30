12fada444428f1723c87e7605fafac10e97afc69
Fabian <fabianjul@gmail.com>
2016-04-30 14:31:50 +0200

    wip

--- sklearn/linear_model/sgd_fast.pyx
+++ sklearn/linear_model/sgd_fast.pyx
@@ -791,7 +791,7 @@ cdef void _test_dot(
     for i in range(n_components):
         print x_data_rbf[i]
 
-    cdef double[:, :] X_2d = np.zeros((1, n_features))
+    cdef np.ndarray[double, ndim = 2, mode='c'] X_2d = np.zeros((1, n_features))
     X_2d[1, :] = X
     real_rbf = rbf.get_RBFSampler()
     x_data_rbf = real_rbf.transform(X)
