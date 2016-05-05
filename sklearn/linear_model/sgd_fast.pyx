1834293cb4936531899141292db5372df0593568
Fabian <fabianjul@gmail.com>
2016-05-05 20:43:20 +0200

    wip

--- sklearn/linear_model/sgd_fast.pyx
+++ sklearn/linear_model/sgd_fast.pyx
@@ -19,6 +19,7 @@ import sys
 from time import time
 
 cimport cython
+from ..utils.extmath cimport safe_sparse_dot  # FJ
 from libc.math cimport exp, log, sqrt, pow, fabs, cos
 cimport numpy as np
 cdef extern from "sgd_fast_helpers.h":
